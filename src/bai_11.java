b614cbd85aa2ce5dfd85dde00c9bb35767fb65ac
DESKTOP-ACRT2MR\User <=>
2023-10-03 16:06:40 +0700

    nop bai

--- src/bai_11.java
+++ src/bai_11.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
@@ -13,9 +14,11 @@ public class bai_11 {
             arr = Integer.parseInt(sc.nextLine());
             stack.push(arr);
         }
-        Collections.sort(stack);
-        while (stack.isEmpty()){
+        Collections.sort(stack,((a,b)->b-a));
+        while (!stack.isEmpty()){
             System.out.println(stack.pop());
         }
+        System.out.println("số lớn nất trong mảng");
+        System.out.println(stack.firstElement());
     }
 }
