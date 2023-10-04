0150ea5e3c117ad6c86b35aa26b959656357007e
DESKTOP-ACRT2MR\User <=>
2023-10-04 17:25:41 +0700

    nop bai

--- src/Bai7.java
+++ src/Bai7.java
@@ -1,5 +1,16 @@
+import java.time.LocalDate;
+import java.time.MonthDay;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class Bai7 {
     public static void main(String[] args) {
-        
+        String chuoiNgayThang = "2023-10-04";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        LocalDate ngayThang = LocalDate.parse(chuoiNgayThang, formatter);
+
+        System.out.println("Ngày tháng: " + ngayThang);
     }
 }
