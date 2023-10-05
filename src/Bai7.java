cda8a2fd2b9611ee7cb76d07226b8ccdcf8cd181
DESKTOP-ACRT2MR\User <=>
2023-10-05 15:21:29 +0700

    nop bai

---
+++ src/Bai7.java
@@ -0,0 +1,20 @@
+import jdk.incubator.vector.VectorOperators;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class Bai7 {
+    public static void main(String[] args) {
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1,"cuong");
+        map.put(2,"cuong1");
+        map.put(3,"cuong2");
+        map.put(4,"cuong3");
+        map.put(5,"cuong4");
+        map.put(6,"cuong5");
+
+
+
+    }
+}
--- src/bai_5.java
+++ src/bai_5.java
@@ -3,20 +3,16 @@ import java.util.function.Consumer;
 
 public class bai_5 {
     public static void main(String[] args) {
-        // Bước 1: Khai báo một HashMap và thêm các phần tử
         Map<String, Integer> hashMap = new HashMap<>();
         hashMap.put("apple", 5);
         hashMap.put("banana", 2);
         hashMap.put("orange", 8);
         hashMap.put("grape", 3);
 
-        // Bước 2: Tạo một danh sách các cặp key-value từ HashMap
         List<Map.Entry<String, Integer>> entryList = new ArrayList<>(hashMap.entrySet());
 
-        // Bước 3: Sắp xếp danh sách các phần tử theo giá trị tăng dần
         Collections.sort(entryList, Map.Entry.comparingByValue());
 
-        // Bước 4: Duyệt qua danh sách đã sắp xếp và in ra
         System.out.println("Các phần tử trong HashMap được sắp xếp theo giá trị tăng dần:");
         for (Map.Entry<String, Integer> entry : entryList) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
